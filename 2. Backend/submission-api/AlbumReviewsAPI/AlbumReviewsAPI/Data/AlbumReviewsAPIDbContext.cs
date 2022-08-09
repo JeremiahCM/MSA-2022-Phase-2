185240f29b704db95e8f015c447aa29674f92b6a
Jeremiah Martinez <itsjeremiahmartinez@gmail.com>
2022-08-09 22:16:00 +1200

    Set up Album Review Model and In-memory Database
    - Created model for API entries titled 'AlbumReview'
    - Created database context for in-memory database

---
+++ 2. Backend/submission-api/AlbumReviewsAPI/AlbumReviewsAPI/Data/AlbumReviewsAPIDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AlbumReviewsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlbumReviewsAPI.Data
+{
+    public class AlbumReviewsAPIDbContext : DbContext
+    {
+        public AlbumReviewsAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AlbumReview> AlbumReviews { get; set; }
+    }
+}
---
+++ 2. Backend/submission-api/AlbumReviewsAPI/AlbumReviewsAPI/Models/AlbumReview.cs
@@ -0,0 +1,10 @@
+﻿namespace AlbumReviewsAPI.Models
+{
+    public class AlbumReview
+    {
+        public Guid Id { get; set; }
+        public string ArtistName { get; set; }
+        public string AlbumName { get; set; }
+        public string Review { get; set; }
+    }
+}
